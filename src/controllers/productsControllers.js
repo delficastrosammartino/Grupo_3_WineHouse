9f8a67dff4aaf4e67188b7669bed15c3daa3245e
matialba22 <albarracinmatias9@gmail.com>
2022-11-09 03:11:51 -0300

    css y html pag de productos

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -1,70 +1,68 @@
 // libreria de node para leer y obtener archivos.
-const fs = require('fs');
+const fs = require("fs");
 // libreria para concatenar y obtener rutas.
-const path = require('path');
-const { validationResult } = require ("express-validator")
+const path = require("path");
+const { validationResult } = require("express-validator");
 
 // indico la ruta de mi archivo .json, la abosulta.
-const productsFilePath = path.join(__dirname, '../data/productsDB.json');
+const productsFilePath = path.join(__dirname, "../data/productsDB.json");
 // primero lee el archivo mencionado en la linea de arriba y la guardo en un array. Guardo en un array los products del .json.
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const productsControllers = {
-    products : (req, res) => {
-        res.render('./products/products', {products});
-    },
+  products: (req, res) => {
+    res.render("./products/products", { products });
+  },
 
-    detalles : (req, res) => {
-        product = products.find(product => product.id == req.params.id)
+  detalles: (req, res) => {
+    product = products.find((product) => product.id == req.params.id);
 
-        res.render('./products/detalles', {product});
-    },
+    res.render("./products/detalles", { product });
+  },
 
-    create : (req, res) => {
-        res.render('./products/crear-producto')
-    },
+  create: (req, res) => {
+    res.render("./products/crear-producto");
+  },
 
-    store : (req, res) => {
-        // resultValidation es un objeto que tiene una propiedad errors usada abajo.
-       const resultValidation = validationResult(req);
-       // si hay errores entra aca.
-          if(resultValidation.errors.length > 0){
-       // renderizo la vista registro, y le paso los errores.
-               return res.render ('./products/crear-producto', {
-       // uso el .mapped para que cada elemento (nombre, apellido, email y password) sea un elemento del objeto y tenga sus propiedades dentro.           
-                   errors: resultValidation.mapped(),
-                   oldData: req.body
-               })
-          }
-          // Creo la variable nuevo producto, es importante que id sea unico e irrepetible.
-          let newProduct = {
-            id: Date.now(),
-            size: Number(req.body.size),
-            image: req.file.filename,
-            name: req.body.name,
-            price: Number(req.body.price),
-            category: req.body.category,
-            discount: Number(req.body.discount),
-            bodega: req.body.bodega,
-            provincia : req.body.provincia,
-            description : req.body.description,
-            stock : true
-        }
+  store: (req, res) => {
+    // resultValidation es un objeto que tiene una propiedad errors usada abajo.
+    const resultValidation = validationResult(req);
+    // si hay errores entra aca.
+    if (resultValidation.errors.length > 0) {
+      // renderizo la vista registro, y le paso los errores.
+      return res.render("./products/crear-producto", {
+        // uso el .mapped para que cada elemento (nombre, apellido, email y password) sea un elemento del objeto y tenga sus propiedades dentro.
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+      });
+    }
+    // Creo la variable nuevo producto, es importante que id sea unico e irrepetible.
+    let newProduct = {
+      id: Date.now(),
+      size: Number(req.body.size),
+      image: req.file.filename,
+      name: req.body.name,
+      price: Number(req.body.price),
+      category: req.body.category,
+      discount: Number(req.body.discount),
+      bodega: req.body.bodega,
+      provincia: req.body.provincia,
+      description: req.body.description,
+      stock: true,
+    };
     // Pusheo en el array products
-        products.push(newProduct)
+    products.push(newProduct);
     // Lo guardo en productsFilePath es la ruta que puse mas arriba, lo convierto en string para poder guardar en el json, eso hace el stringify
     // Le mando products, el null y el " " son para orden, salto de linea o algo asi.
-        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "))
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
     // vulevo a /products
-        res.redirect("/products")
-    },
- 
-    edit : (req, res) => {
-        let productToEdit = products.find(product => product.id == req.params.id);
-		res.render('editar-producto', {productToEdit})
-    }
+    res.redirect("/products");
+  },
 
-}
+  edit: (req, res) => {
+    let productToEdit = products.find((product) => product.id == req.params.id);
+    res.render("editar-producto", { productToEdit });
+  },
+};
 
 module.exports = productsControllers;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,29 +1,29 @@
 // ************ Require's ************
-const express = require('express');
-const multer = require('multer');
+const express = require("express");
+const multer = require("multer");
 const router = express.Router();
-const path = require('path')
-const {body} = require('express-validator');
+const path = require("path");
+const { body } = require("express-validator");
 
 // ************ Proceso para manejar archivos ************
 // creo el storage donde se va a guardar la info.
 const storage = multer.diskStorage({
-    // Ubicacion
-        destination: (req, file, cb) => {
-            cb(null, "./public/images/productImages")
-        },
-    // Nombre
-        filename: (req, file, cb) => {
-    // numero unico con el Date.now, un _img y la extension del archivo original. 
-            let fileName = Date.now() + "_img" + path.extname(file.originalname)
-            cb(null, fileName)
-        }
-    })
-    
-    // Constatnte donde genero el metodo a usar para manejar archivos
-    const uploadFile = multer ({ storage })
+  // Ubicacion
+  destination: (req, file, cb) => {
+    cb(null, "./public/images/productImages");
+  },
+  // Nombre
+  filename: (req, file, cb) => {
+    // numero unico con el Date.now, un _img y la extension del archivo original.
+    let fileName = Date.now() + "_img" + path.extname(file.originalname);
+    cb(null, fileName);
+  },
+});
 
-    // validacion de express-validation con la funcion body.
+// Constatnte donde genero el metodo a usar para manejar archivos
+const uploadFile = multer({ storage });
+
+// validacion de express-validation con la funcion body.
 // const validations = [
 //     body("name")
 //         .notEmpty().withMessage("Tienes que indicar el nombre del producto"),
@@ -47,15 +47,14 @@ const storage = multer.diskStorage({
 
 // ************ Controller Require ************
 // establece los metodos para operar cada una de las rutas, la logica para resolver rutas.
-const productsControllers = require('../controllers/productsControllers');
+const productsControllers = require("../controllers/productsControllers");
 
-router.get('/', productsControllers.products);
+router.get("/", productsControllers.products);
 
-router.get('/detalles/:id', productsControllers.detalles);
+router.get("/detalles/:id", productsControllers.detalles);
 
-router.get('/crear', productsControllers.create);
+router.get("/crear", productsControllers.create);
 
-router.post('/', uploadFile.single('image'), productsControllers.store)
-
+router.post("/", uploadFile.single("image"), productsControllers.store);
 
 module.exports = router;
