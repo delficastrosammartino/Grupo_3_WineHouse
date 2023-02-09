c3c1ac7e88280e33668a4a045fed1af1719ba2b6
estanis22 <estanislaoperezpesado@gmail.com>
2023-02-09 15:51:57 -0300

    armando el boton de cantidad en la vista detalles

---
+++ public/js/detalles-validation.js
@@ -0,0 +1,19 @@
+const decrementButton = document.querySelector('.decrement');
+const incrementButton = document.querySelector('.increment');
+const quantityInput = document.querySelector('input[type="text"]');
+
+decrementButton.addEventListener('click', function() {
+  let quantity = parseInt(quantityInput.value);
+  if (quantity > 1) {
+    quantity--;
+  }
+  quantityInput.value = quantity;
+});
+
+incrementButton.addEventListener('click', function() {
+  let quantity = parseInt(quantityInput.value);
+  if (quantity < 24) {
+    quantity++;
+  }
+  quantityInput.value = quantity;
+});
